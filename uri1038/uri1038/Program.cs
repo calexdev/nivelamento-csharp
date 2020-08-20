a75d35d770a5a08f832561088dcd5b4fa9385b2c
Carlos Alexandre <47224339+calexdev@users.noreply.github.com>
2020-08-20 20:31:05 -0300

    Update Program.cs
    Apenas uma adição de linhas (8 a 16) para que fique um pouco mais didático e fácil de entender o que está sendo digitado.

--- uri1038/uri1038/Program.cs
+++ uri1038/uri1038/Program.cs
@@ -5,6 +5,16 @@ namespace uri1038 {
     class Program {
         static void Main(string[] args) {
 
+            Console.WriteLine("|------------------------------------|");
+            Console.WriteLine("| Codigo | Expecificação   |  Preço  |");
+            Console.WriteLine("| 1      | Cachorro quente |  R$4.00 |");
+            Console.WriteLine("| 2      | X-Salada        |  R$4.50 |");
+            Console.WriteLine("| 3      | X-Bacon         |  R$5.00 |");
+            Console.WriteLine("| 4      | Torrada Simples |  R$2.00 |");
+            Console.WriteLine("| 5      | Refrigerante    |  R$1.50 |");
+            Console.WriteLine("|------------------------------------|");
+            Console.WriteLine("Digite o codigo do produto e a quantidade que deseja para saber o valor da compra: ");
+            
             string[] valores = Console.ReadLine().Split(' ');
             int codigo = int.Parse(valores[0]);
             int quantidade = int.Parse(valores[1]);
